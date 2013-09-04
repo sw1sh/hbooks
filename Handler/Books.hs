cdc07e62dc76fd570edb9f3b17b751f78d5a0573
sw1sh <murzin.nikolay@physics.msu.ru>
2013-09-05 02:18:02 +0400

    File upload from local or url

--- Handler/Books.hs
+++ Handler/Books.hs
@@ -1,8 +1,13 @@
-{-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
+{-# LANGUAGE OverloadedStrings, ScopedTypeVariables, ViewPatterns #-}
 
 module Handler.Books where
 
 import Import
+import Data.Conduit (runResourceT, ($$))
+import Data.Conduit.Binary (sinkLbs)
+import Network.Wai
+import Network.HTTP.Types.Status
+import Network.HTTP.Types.Header
 
 getBooksR :: Handler Html
 getBooksR = do
@@ -12,31 +17,53 @@ getBooksR = do
 getAddBookR :: Handler Html
 getAddBookR = do
   ((res, form), enctype) <- runFormPost bookForm
+  let reshowForm = defaultLayout $(widgetFile "bookform")
   case res of
-    FormSuccess (title, cover) -> do
-      let file = fileName cover
-      liftIO $ toThumbnail cover
-      cid <- runDB $ insert $ Cover file
-      bid <- runDB $ insert $ Book title cid
-      redirect $ BookR bid
-    _ -> defaultLayout $(widgetFile "bookform")
+    FormSuccess (title, cover, maybeFile, maybeUrl) -> do
+      case maybeFile of
+        Just file -> do 
+          cid <- runDB $ insert $ Cover $ fileName cover
+          liftIO $ saveThumbnail cover $ showId cid
+          bid <- runDB $ insert $ Book title cid (fromString . unpack $ fileContentType file)
+          content <- runResourceT $ fileSource file $$ sinkLbs
+          liftIO $ saveFile (repack content) (showId bid)
+          redirect $ BookR bid
+        Nothing -> 
+          case maybeUrl of
+            Just url -> do
+              res <- liftIO $ downloadFile url
+              case res of
+                Right (contentType, body) -> do
+                  cid <- runDB $ insert $ Cover $ fileName cover
+                  liftIO $ saveThumbnail cover $ showId cid
+                  bid <- runDB $ insert $ Book title cid (fromString contentType)
+                  liftIO $ saveFile (fromString body) $ showId bid
+                  redirect $ BookR bid
+                Left err -> reshowForm
+            Nothing -> reshowForm
+    _ -> reshowForm
 
 postAddBookR :: Handler Html
 postAddBookR = getAddBookR
 
 getBookR :: BookId -> Handler Html
 getBookR bid = do
-  Book title cid <- runDB $ get404 bid
-  maybeCover <- runDB $ get cid
-  let imageSrc = 
-        case maybeCover of
-          Just (Cover imageFileName) -> "/static/img/covers/" ++ imageFileName
-          Nothing -> ""
+  Book title cid (show -> contentType :: Text) <- runDB $ get404 bid
+  let imageSrc = "/static/img/covers/" ++ showId cid :: Text
+      fileUrl = "/download/" ++ showId bid :: Text
   defaultLayout $(widgetFile "book")
       
-  
+getDownloadR :: BookId -> Handler Html
+getDownloadR bid = do
+  Book title _ contentType <- runDB $ get404 bid
+  let file = ResponseFile ok200 
+        [("Content-Disposition", "filename=" ++ encodeUtf8 title ), (hContentType, contentType)] 
+        ("static/books/" ++ showId bid) Nothing
+  sendWaiResponse file
 
-bookForm :: Form (Text,FileInfo)
-bookForm = renderDivs $ (,)
+bookForm :: Form (Text,FileInfo,Maybe FileInfo,Maybe Text)
+bookForm = renderDivs $ (,,,)
     <$> areq textField "Title" Nothing
     <*> fileAFormReq "Cover image"
+    <*> fileAFormOpt "Upload file"
+    <*> aopt textField "File URL" Nothing
--- Model.hs
+++ Model.hs
@@ -1,19 +1,38 @@
 module Model where
 
 import ClassyPrelude
+import ClassyPrelude.Classes
 import Yesod
 import Database.Persist.Quasi
 import Data.Conduit
 import Data.Conduit.Binary
 import Graphics.ImageMagick.MagickWand
+import Network.Curl
 
-toThumbnail :: FileInfo -> IO ()
-toThumbnail f = withMagickWandGenesis $ do
+showPersist (PersistInt64 i) = show i
+showPersist _ = undefined 
+
+showId = showPersist . unKey
+
+saveThumbnail :: FileInfo -> FilePath ->  IO ()
+saveThumbnail file name = withMagickWandGenesis $ do
   (_,w) <- magickWand
-  blob <- runResourceT $ fileSource f $$ sinkLbs
+  blob <- runResourceT $ fileSource file $$ sinkLbs
   readImageBlob w $ repack blob
-  resizeImage w 150 120 lanczosFilter 1
-  writeImage w $ Just $ "static/img/" </> repack (fileName f)
+  extentImage w 120 150 0 0
+  writeImage w $ Just $ "static/img/covers" </> name
+
+saveFile :: ByteString -> FilePath ->  IO ()
+saveFile body name = writeFile (repack $  "static/books" </> name) body
+
+downloadFile :: Text -> IO (Either String (String,String))
+downloadFile url = do
+  resp <- curlGetResponse (unpack url) []
+  IDouble size <- respGetInfo resp SizeDownload
+  IString contentType <- respGetInfo resp ContentType
+  case respCurlCode resp of
+    CurlOK -> return $ Right (contentType, respBody resp)
+    err -> return $ Left $ show err
 
 -- You can define all of your database entities in the entities file.
 -- You can find more information on persistent and how to declare entities
